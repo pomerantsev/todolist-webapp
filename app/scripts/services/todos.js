7c08803cce682fef8a8ba67d214717156d581037
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-07 21:34:12 +0400

    Be more explicit about the ENV constant

--- app/scripts/services/todos.js
+++ app/scripts/services/todos.js
@@ -4,7 +4,7 @@ app.factory('Todos', function($resource, ENV) {
   var backend;
   if (ENV === 'production') {
     backend = 'http://todolist-api.herokuapp.com'
-  } else {
+  } else if (ENV === 'development') {
     backend = 'http://localhost:3000'
   }
   return $resource(backend + '/todos/:id')
