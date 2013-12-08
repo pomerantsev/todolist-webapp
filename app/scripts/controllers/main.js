8a41ae34b3cd1392daf87657963dcb27659fa21d
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-08 10:53:38 +0400

    Add a form for creating new todos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,14 @@
 angular.module('myTodolistWebfrontApp')
   .controller('MainCtrl', function ($scope, Todos) {
     $scope.todos = Todos.query();
+
+    $scope.create = function(title) {
+      Todos.save(
+        {todo:
+          {title: title, completed: false, priority: 2}
+        }, function(todo) {
+          $scope.todos.push(todo);
+          $scope.title = "";
+        });
+    }
   });
